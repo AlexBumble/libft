2df436654e6c954dd9f5e4a67a5c3831cecdfbfa
Бузов Алексей Александрович <AAlBuzov@sberbank.ru>
2020-11-02 21:26:55 +0300

    ft_strnstr

---
+++ ft_strnstr.c
@@ -0,0 +1,19 @@
+#include "libft.h"
+
+char *ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+    int i;
+    i = 0;
+
+    while(i <= len && needle[i] && haystack[i])
+    {
+        if (haystack[i] != needle[i])
+            return ft_strnstr(haystack + i + 1, needle, len);
+        else if (haystack[i] == needle[i])
+            i++;
+
+        if (!needle[i] && i < len)
+            return (char *)haystack;
+    }
+    return ft_strlen(needle) == 0 ? (char *)haystack : NULL;
+}
